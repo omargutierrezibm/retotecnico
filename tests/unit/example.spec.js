210f27362ed56c0d3533cc0c4a3e18ab801b092d
Omar <omar.gutierrez.cl@ibm.com>
2022-08-30 19:57:27 -0500

    prueba unitaria

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,12 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils'
+import Home from '@/views/Home-component.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('Home-component.vue', () => {
+  it('ingreso de texto en input', async () => {
+    const wrapper = mount(Home)
+    wrapper.vm.newTodo = 'nueva tarea'
+    await wrapper.trigger('keydown.enter')
+    expect(wrapper.vm.newTodo).toBe('nueva tarea')
   })
 })
